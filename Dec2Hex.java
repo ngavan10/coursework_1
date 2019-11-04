b01d0f54836ed653d04a5ea6e31a6f22b1355d70
ngavan10 <56032771+ngavan10@users.noreply.github.com>
2019-11-04 19:36:42 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -16,12 +16,34 @@ class Dec2Hex
             }
         }
 
-        HexNum hex = new HexNum(Arg1);
-        hex.getNum();
-        hex.displayNum();
-        hex.getHex();
-        hex.displayHex();
+        
+    char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    int rem, num;
+    String hexadecimal="";
 
+
+
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+
+
+
+        while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
+        }
+       
+
+
+    
+
+  
+
+        System.out.println("Hexadecimal representation is : " + hexadecimal);
+
+  
     }
 
 
