bdd6d4fd7d9087502bfc2d3cfcf11a61d83a5308
ngavan10 <56032771+ngavan10@users.noreply.github.com>
2019-11-04 19:47:25 +0000

    Add files via upload

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Dec2Hex
+public class Dec2Hex
 {
 
     public static int Arg1;
@@ -16,36 +16,14 @@ class Dec2Hex
             }
         }
 
-        
-    char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    int rem, num;
-        num = Arg1;
-    String hexadecimal="";
+        HexNum hex = new HexNum(Arg1);
+        hex.getNum();
+        hex.displayNum();
+        hex.getHex();
+        hex.displayHex();
 
-
-
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-
-
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-       
-
-
-    
-
-  
-
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
-
-  
     }
 
 
 }
+
---
+++ HexNumTest.java
@@ -0,0 +1,35 @@
+import junit.framework.TestCase;
+
+import static junit.framework.Assert.assertEquals;
+//import static org.junit.jupiter.api.Assertions.*;
+
+public class HexNumTest extends TestCase {
+
+    //private HexNum hex_num = new HexNum(100);
+    //@org.junit.jupiter.api.Test
+    public void testGetNumOf100()
+    {
+        HexNum hex_num = new HexNum(100);
+        assertEquals(hex_num.getNum(), 100);
+    }
+
+    public void testGetNumOf50()
+    {
+        HexNum hex_num = new HexNum(50);
+        assertEquals(hex_num.getNum(), 50);
+    }
+
+    //@org.junit.jupiter.api.Test
+    public void testGetHexOf100()
+    {
+        HexNum hex_num = new HexNum(100);
+        assertEquals(hex_num.getHex(), "64");
+    }
+
+    public void testGetHexOf50()
+    {
+        HexNum hex_num = new HexNum(50);
+        assertEquals(hex_num.getHex(), "32");
+    }
+    
+}
