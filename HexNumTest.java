1673bcecda4648d5e2ffb43af24916bf81125f98
ngavan10 <56032771+ngavan10@users.noreply.github.com>
2019-11-05 14:03:13 +0000

    Update HexNumTest.java

--- HexNumTest.java
+++ HexNumTest.java
@@ -1,32 +1,57 @@
-import junit.framework.TestCase;
 
-public class HexNumTest extends TestCase {
 
-    //private HexNum hex_num = new HexNum(100);
-    //@org.junit.jupiter.api.Test
+public class HexNumTest {
+
+
+
+
+    public static void main(String args[])
+    {
+        HexNumTest test = new HexNumTest();
+        test.testGetNumOf100();
+        test.testGetNumOf50();
+        test.testGetHexOf100();
+        test.testGetHexOf50();
+
+    }
+
     public void testGetNumOf100()
     {
         HexNum hex_num = new HexNum(100);
-        assertEquals(hex_num.getNum(), 100);
+        if(hex_num.getNum() == 100)
+        {
+            System.out.println("Test for get Num of 100 Success");
+        }
+
     }
 
     public void testGetNumOf50()
     {
+
         HexNum hex_num = new HexNum(50);
-        assertEquals(hex_num.getNum(), 50);
+        if(hex_num.getNum() == 50)
+        {
+            System.out.println("Test for get Num of 50 Success");
+        }
     }
 
-    //@org.junit.jupiter.api.Test
+
     public void testGetHexOf100()
     {
         HexNum hex_num = new HexNum(100);
-        assertEquals(hex_num.getHex(), "64");
+        if(hex_num.getHex().equals("64"))
+        {
+            System.out.println("Test for get Hex of 100 Success");
+        }
     }
 
     public void testGetHexOf50()
     {
         HexNum hex_num = new HexNum(50);
-        assertEquals(hex_num.getHex(), "32");
+        if(hex_num.getHex().equals("32"))
+        {
+            System.out.println("Test for get Hex of 50 Success");
+        }
     }
     
 }
